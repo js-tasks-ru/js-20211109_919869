203661d519216a9b5df86f64a93e5b686ee412d0
919869 <919869@outlook.com>
2021-11-19 09:50:15 +0300

    Выполнены упражнения лекции №3

--- 03-objects-arrays-intro-to-testing/1-create-getter/index.js
+++ 03-objects-arrays-intro-to-testing/1-create-getter/index.js
@@ -4,5 +4,31 @@
  * @returns {function} - function-getter which allow get value from object by set path
  */
 export function createGetter(path) {
+    const keys = path.split('.').reverse();
 
+    return function (obj) {
+        let result = null;
+
+        if (keys.length > 0) {
+            result = getKeyValue(keys, obj);
+        }
+
+        return result;
+    }
+
+    function getKeyValue(keys, obj) {
+        const currKey = keys.pop();
+
+        if (obj === undefined) {
+            return;
+        } else if (currKey === undefined) {
+            return obj;
+        } else {
+            for (const [key, value] of Object.entries(obj)) {
+                if (currKey === key) {
+                    return getKeyValue(keys, value);
+                }
+            }
+        }
+    }
 }
--- 03-objects-arrays-intro-to-testing/2-invert-object/index.js
+++ 03-objects-arrays-intro-to-testing/2-invert-object/index.js
@@ -4,5 +4,13 @@
  * @returns {object | undefined} - returns new object or undefined if nothing did't pass
  */
 export function invertObj(obj) {
+    const invertMap = new Map();
 
+    if (obj === undefined) return; 
+    
+    for (const [key, value] of Object.entries(obj)) {
+        invertMap.set(value, key);
+    }
+
+    return Object.fromEntries(invertMap);
 }
